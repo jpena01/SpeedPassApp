c32ce02858808fa936d488e5b6076e86e56c4dfd
Alexander_Dev1337 <joshuap@12321@gmail.com>
2023-10-02 13:52:54 -0400

    #Fixing auth issue

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SpeedPassApp.Data.SpeedPassApp;
+using Microsoft.EntityFrameworkCore;
 
 namespace SpeedPassApp
 {
