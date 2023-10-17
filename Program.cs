e4ccf46504441ebfecf0d3d950118e7156b03d5f
Alexander_Dev1337 <joshuap@12321@gmail.com>
2023-10-17 11:52:28 -0400

    #fixed many issues with Web host. Nearing v1!

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace SpeedPassApp
 {
@@ -11,29 +11,76 @@ namespace SpeedPassApp
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureServices((hostContext, services) =>
+        public class Startup
+        {
+            public Startup(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+
+            public IConfiguration Configuration { get; }
+
+            // This method gets called by the runtime. Use this method to add services to the container.
+            public void ConfigureServices(IServiceCollection services)
+            {
+                // Add services to the container
+                services.AddControllersWithViews();
+            }
+
+            public static void Main(string[] args)
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
                     {
-                        services.AddDbContext<AppDbContext>(options =>
-                            options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
-
-                        services.AddRazorPages();
-                    });
-
-                    webBuilder.Configure((app) =>
-                    {
-                        app.UseRouting();
-
-                        app.UseStaticFiles(); // Add this line to serve static files from the wwwroot folder
-                        app.UseEndpoints(endpoints =>
+                        webBuilder.UseStartup<Startup>();
+                        webBuilder.UseKestrel(options =>
                         {
-                            endpoints.MapRazorPages();
+                            options.Listen(System.Net.IPAddress.Parse("192.168.1.41"), 80); // Specify your IP and port
                         });
                     });
-                });
-        
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.ConfigureServices((hostContext, services) =>
+                        {
+                            services.AddDbContext<AppDbContext>(options =>
+                                options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
+
+                            services.AddRazorPages();
+                        });
+
+                        webBuilder.Configure((app) =>
+                        {
+                            app.UseRouting();
+
+                            app.UseStaticFiles(); // Add this line to serve static files from the wwwroot folder
+                            app.UseEndpoints(endpoints =>
+                            {
+                                endpoints.MapRazorPages();
+                            });
+                        });
+                    });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+               WebHost.CreateDefaultBuilder(args)
+                   .UseKestrel(options =>
+                   {
+                       // Listen on specific IP and port
+                       options.Listen(IPAddress.Parse("192.168.1.41"), 5000); // Port 80 for HTTP
+
+                       // Add a binding for partsmaxinc.co
+                       options.Listen(IPAddress.Parse("192.168.1.41"), 5000, listenOptions =>
+                       {
+                           listenOptions.UseConnectionLogging(); // Enable connection logging
+                       });
+                   })
+                   .UseStartup<Startup>();
+        //Kestrel settings here:
     }
 }
